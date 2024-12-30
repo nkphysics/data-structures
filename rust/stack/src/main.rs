37a8d308854e41394d886ae83112b6ce5f2da33f
Tsar Bomba Nick <njkuechel@protonmail.com>
2024-12-29 23:34:04 -0600

    Added destructor to rust Stack example + some extra notes

--- rust/stack/src/main.rs
+++ rust/stack/src/main.rs
@@ -1,6 +1,8 @@
 // Reference: https://rust-unofficial.github.io/too-many-lists/third.html
+// RC reference: https://doc.rust-lang.org/rust-by-example/std/rc.html
 
 use std::rc::Rc;
+// Reference counting pointer (RC = reference counted)
 
 pub struct Stack<T> {
     head: Link<T>,
@@ -22,6 +24,7 @@ impl<T> Stack<T> {
     }
 
     pub fn push(&self, elem: T) -> Stack<T> {
+        // 0(1) linear time
         Stack { head: Some(Rc::new(Node {
             elem: elem,
             next: self.head.clone(),
@@ -31,6 +34,7 @@ impl<T> Stack<T> {
     }
 
     pub fn pop(&self) -> Stack<T> {
+        // 0(1) linear time
         if self.size > 0 {
             Stack { head: self.head.as_ref().and_then(|node| node.next.clone()),
                     size: self.size - 1 }
@@ -41,7 +45,23 @@ impl<T> Stack<T> {
     }
 
     pub fn peek(&self) -> Option<&T> {
+        // 0(1) linear time
         self.head.as_ref().map(|node| &node.elem)
+    }
+}
+
+impl<T> Drop for Stack<T> {
+    // Destructor for the Stack
+    // Reference on destructors: https://doc.rust-lang.org/stable/reference/destructors.html
+    fn drop(&mut self) {
+        let mut head = self.head.take();
+        while let Some(node) = head {
+            if let Ok(mut node) = Rc::try_unwrap(node) {
+                head = node.next.take();
+            } else {
+                break;
+            }
+        }
     }
 }
 
